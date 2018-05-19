74f5bf93ad14189603e93dc5a205271f4bd76722
Bogdan Orzea <bogdan.orzea@gmail.com>
2018-05-19 17:17:55 +0300

    Link to the correct input file

--- src/Program.java
+++ src/Program.java
@@ -9,7 +9,7 @@ public class Program {
 
         System.out.println("Reading file from disk...");
 
-        try (FileReader fileReader = new FileReader("resources/input3.txt")) {
+        try (FileReader fileReader = new FileReader("resources/input.txt")) {
             BufferedReader bufferedReader = new BufferedReader(fileReader);
             Lexer lexer = new Lexer(bufferedReader);
             Token token = lexer.getToken();
